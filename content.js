180c342e98962ffad87db109349e0ecb993987bd
andile <guguandilemlata@gmail.com>
2019-01-08 23:02:11 +0200

    works on the home page

--- content.js
+++ content.js
@@ -1,17 +1,10 @@
-console.log(window.location.host);
+//remove all scratched out prices and % signs
+//Todo figure out how to do this on page load
+$(".my-saving").remove();
+$(".group.wg").remove();
 
-if (window.location.host === "www.takealot.com") {
-  //remove all scratched out prices and % signs
-
-  var paras = document.getElementsByClassName(
-    "my-saving __web-inspector-hide-shortcut__"
-  );
-
-  while (paras[0]) {
-    paras[0].parentNode.removeChild(paras[0]);
-  }
-}
-
+// badge saving
 //<div class="my-saving __web-inspector-hide-shortcut__">27% Off</div>
 /* <p class="price-was list-price-info __web-inspector-hide-shortcut__"><del class="old-price">R 184</del></p> */
 /* <span class="list-price-info-icon list-price-info __web-inspector-hide-shortcut__" deluminate_imagetype="unknown"></span> */
+// <p class="deal-qty lt-10 __web-inspector-hide-shortcut__" data-reactid=".0.2.2.a.3.1"><span data-reactid=".0.2.2.a.3.1.0">Only </span><span class="qty-count" data-reactid=".0.2.2.a.3.1.1">13</span><span data-reactid=".0.2.2.a.3.1.2"> Left</span></p>
